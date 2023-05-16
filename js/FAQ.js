fd45d5e7942365c1e5701bef43e9516fd1b1527a
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-05-16 20:43:21 +0100

    complete FAQ page

--- js/FAQ.js
+++ js/FAQ.js
@@ -3,11 +3,15 @@ const q = document.querySelectorAll('.q');
 console.log(q.children);
 q.forEach((ele)=>{
     ele.children[0].onclick = ()=>{
-        // q.forEach((elehide)=>{
-        //     elehide.children[1].style.height = '0px';
-        //     elehide.classList.remove('color');
-        // });
+        q.forEach((e)=>{
+            if(ele!== e){
+                e.classList.remove('color');
+                e.children[1].style.height = `0`
+            }
+        });
         ele.classList.toggle('color');
         ele.classList.contains('color')?ele.children[1].style.height = `${ele.children[1].scrollHeight}px`:ele.children[1].style.height = `0px`;
     }
-})
+});
+const askQues = document.querySelector('.ask-que');
+console.log(askQues.getBoundingClientRect());
