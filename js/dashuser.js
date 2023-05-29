e3c930a0f7f8b7b5edb75b60d0a02dfbd7498d73
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-05-29 21:01:21 +0100

    complete some changes in html and js clicking buttons

--- js/dashuser.js
+++ js/dashuser.js
@@ -7,3 +7,39 @@ lis.forEach((ele)=>{
         ele.classList.add('active');
     }
 });
+// buttons save and close
+const close  = document.querySelectorAll('#close');
+const save_chnages  = document.querySelector('#save');
+const body = document.querySelector('body');
+console.log(close)
+console.log(save_chnages);
+// all butons in dashboard page
+let all_buttons = document.querySelectorAll('#edit');
+all_buttons.forEach((btn)=>{
+    btn.onclick = ()=>{
+        body.classList.add('overlay');
+        document.querySelector(".myforms").classList.add('tr-lt');
+    };
+});
+// save changes when click at save
+save_chnages.onclick = ()=>{
+    // save changes
+    body.classList.remove('overlay');
+    document.querySelector(".myforms").classList.remove('tr-lt');
+}
+// close this page when click at close button
+close.forEach((btnclose)=>{
+    btnclose.onclick = ()=>{
+        // no save any changes
+        body.classList.remove('overlay');
+        document.querySelector(".myforms").classList.remove('tr-lt');
+        // when add call back function to create elements using JS
+        // setTimeout(()=>{
+        //     document.querySelector(".myforms").remove();
+        // },500);
+}
+});
+// when click at send password create div of change password page using js 
+document.querySelector("#ch-password").onclick =()=>{
+    console.log('create page password here using JS');
+}
