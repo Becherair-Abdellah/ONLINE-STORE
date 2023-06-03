2cdd796ee1dabb4f74ea574a3aea2f5c0bc1ed74
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-06-03 13:44:04 +0100

    complete dashboard page now

--- js/dashuser.js
+++ js/dashuser.js
@@ -14,39 +14,70 @@ const my_body = document.querySelector('body');
 const pesudo = document.querySelector('.pesudo');
 const edits = document.querySelectorAll('#edit');
 const contnet_show = document.querySelector('.myforms');
-edits.forEach((ele)=>{
-    ele.onclick = ()=>{
+edits.forEach((ele) => {
+    ele.onclick = () => {
         contnet_show.classList.add('bl-imp');
         my_body.classList.add('overflow-hidden');
         pesudo.classList.add('myoverlay');
-        setTimeout(()=>{
+        setTimeout(() => {
             contnet_show.classList.add('animate');
-        },100);
+        }, 100);
+        pesudo.onclick = ()=>{
+            contnet_show.classList.remove('animate');
+            pesudo.classList.remove('myoverlay');
+            setTimeout(()=>{
+            contnet_show.classList.remove('bl-imp');
+            my_body.classList.remove('overflow-hidden');
+
+            },300);
+        }
     };
 });
 // close buttons at forms
 const all_close = document.querySelectorAll('#close');
 console.log(all_close);
-all_close.forEach((ele)=>{
-    ele.onclick = ()=>{
-       // for mobile screen and tablet and laptop
-       if(window.innerWidth<1280){
-             contnet_show.classList.remove('bl-imp');
-      }
-       else{
-        setTimeout(()=>{
+all_close.forEach((ele) => {
+    ele.onclick = () => {
+        // for mobile screen and tablet and laptop
+        if (window.innerWidth < 1280) {
             contnet_show.classList.remove('bl-imp');
-           },500);
-       }
+        }
+        else {
+            setTimeout(() => {
+                contnet_show.classList.remove('bl-imp');
+            }, 500);
+        }
         my_body.classList.remove('overflow-hidden');
         pesudo.classList.remove('myoverlay');
         contnet_show.classList.remove('animate');
-        
+
     }
 });
 // button show more
 const show_more = document.querySelector('#show-more');
 const profile_contnet = document.querySelector('.pro-file');
-show_more.onclick = ()=>{
+show_more.onclick = () => {
     profile_contnet.classList.add('l-0');
+    my_body.classList.add('overflow-hidden');
+    pesudo.classList.add('myoverlay');
+    // create button close tab
+    let creat_btn = document.createElement('button');
+    creat_btn.innerText = 'x';
+    creat_btn.classList = 'btnclose';
+    document.querySelector('.info').appendChild(creat_btn);
+    // close tab contnet
+    let eles = [creat_btn,pesudo];
+    eles.forEach((ele)=>{
+        ele.onclick = ()=>{
+            my_body.classList.remove('overflow-hidden');
+            pesudo.classList.remove('myoverlay');
+            profile_contnet.classList.remove('l-0');
+        }
+    });
+    //setting some changes about screen height 
+if(window.innerHeight<=628){
+      
+    document.querySelector('.tools').classList.add('h-[308px]','overflow-hidden','overflow-y-visible');
+    console.log('run time');
+}
 };
